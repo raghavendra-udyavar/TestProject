4f74d34bd90a9ecdb8e04784fa98c9f1de77b09f
Raghavendra Udyavar <raghavendra@mvpsports.life>
2018-12-15 14:22:39 +0530

    minor refactors to the naming conventions that was followed earlier

---
+++ Assets/Scripts/Parser/CubeEffectDataParser.cs
@@ -0,0 +1,14 @@
+﻿using LAIGames.Parser.Interface;
+using LAIGames.Gameplay;
+using UnityEngine;
+
+namespace LAIGames.Parser
+{
+    public class CubeEffectDataParser : IDataDeserializer<CubeEffectData>
+    {
+        public CubeEffectData Deserialize(string jsonString)
+        {
+            return JsonUtility.FromJson<CubeEffectData>(jsonString);
+        }
+    }
+}
